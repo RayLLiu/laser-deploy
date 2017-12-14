af92e47934c7dd52af5dedf73cb4f5732fd34e7f
Rui(Ray) Liu <RayLLiu@users.noreply.github.com>
2017-12-13 21:23:24 -0500

    config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports.serverConfig = {
+  PORT: 8888,
+  repos: [{
+    full_name: '',
+    secret: '',
+    fullPath: '',
+    pm2ProcessID: '0'
+  }],
+  deployAPIendpoint: 'deploy'
+}
+
